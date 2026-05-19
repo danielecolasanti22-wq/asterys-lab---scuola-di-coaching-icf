e77a456ae2131f1e3d2935c52c006e40269e9eac
SIVRA <daniele.colasanti22@gmail.com>
2026-05-19 12:30:10 +0200

    Update CourseDetail.tsx

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -555,6 +555,8 @@ export default function CourseDetail({ courseId, courseData }: CourseDetailProps
                   ? 'right-[-36%] w-[calc(117vw-135px)] max-w-[1755px] min-w-[1215px] -translate-x-[40px]'
                   : id === 'voice-dialogue'
                   ? 'right-[-22%] w-[calc(82vw-65px)] max-w-[1255px] min-w-[875px] translate-x-[20px]'
+                  : id === 'eiw'
+                  ? 'right-[2%] w-[calc(60vw-80px)] max-w-[1040px] min-w-[760px] -translate-x-[60px]'
                   : 'right-[-36%] w-[calc(78vw-80px)] max-w-[1180px] min-w-[820px] translate-x-[150px]'
               }`}
             >
